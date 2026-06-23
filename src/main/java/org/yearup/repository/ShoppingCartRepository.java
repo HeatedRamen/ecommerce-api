08d40b90726e8d02f38cc61c2fd76b5f7b143a68
ramenmac <kevinnguyen4200@gmail.com>
2026-06-23 10:27:11 -0500

    Add functionality to shopping cart

--- src/main/java/org/yearup/repository/ShoppingCartRepository.java
+++ src/main/java/org/yearup/repository/ShoppingCartRepository.java
@@ -1,6 +1,7 @@
 package org.yearup.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.yearup.models.CartItem;
 
@@ -11,7 +12,15 @@ public interface ShoppingCartRepository extends JpaRepository<CartItem, Integer>
 {
     List<CartItem> findByUserId(int userId);
 
-    CartItem findByUserIdAndProductId(int userId, int productId);
+    CartItem findByUserIdAndProduct_ProductId(int userId, int productId);
 
     void deleteByUserId(int userId);
+
+    @Query("""
+            SELECT c
+            FROM CartItem c
+            JOIN FETCH c.product
+            WHERE c.userId = :userId
+            """)
+    List<CartItem> findByUserIdWithProducts(int userId);
 }
